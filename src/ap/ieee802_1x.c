45f3ba658a618540c80a3c185db065bbced8b7b1
Ilan Peer <ilan.peer@intel.com>
2023-05-22 22:33:39 +0300

    AP: MLO: Make IEEE 802.1X SM, authserv, and RADIUS client singletons
    To simplify the handling of MLD stations, assume that all
    interfaces/BSSs use the same IEEE 802.1X authenticator, the same RADIUS
    server instance, and the same RADIUS client.
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Signed-off-by: Andrei Otcheretianski <andrei.otcheretianski@intel.com>
    CRs-Fixed: 3430882
    Git-repo: git://w1.fi/hostap.git
    Git-commit: a213fee11da3f9d3b5fff61c7862f3ad62e753b1
    Change-Id: I1026e811eac92625727cda239d7469ebda169865

--- src/ap/ieee802_1x.c
+++ src/ap/ieee802_1x.c
@@ -2437,6 +2437,14 @@ int ieee802_1x_init(struct hostapd_data *hapd)
 	struct eapol_auth_config conf;
 	struct eapol_auth_cb cb;
 
+	if (hapd->mld_first_bss) {
+		wpa_printf(MSG_DEBUG,
+			   "MLD: Using IEEE 802.1X state machine of the first BSS");
+
+		hapd->eapol_auth = hapd->mld_first_bss->eapol_auth;
+		return 0;
+	}
+
 	dl_list_init(&hapd->erp_keys);
 
 	os_memset(&conf, 0, sizeof(conf));
@@ -2521,6 +2529,14 @@ void ieee802_1x_erp_flush(struct hostapd_data *hapd)
 
 void ieee802_1x_deinit(struct hostapd_data *hapd)
 {
+	if (hapd->mld_first_bss) {
+		wpa_printf(MSG_DEBUG,
+			   "MLD: Deinit IEEE 802.1X state machine of a non-first BSS");
+
+		hapd->eapol_auth = NULL;
+		return;
+	}
+
 #ifdef CONFIG_WEP
 	eloop_cancel_timeout(ieee802_1x_rekey, hapd, NULL);
 #endif /* CONFIG_WEP */
