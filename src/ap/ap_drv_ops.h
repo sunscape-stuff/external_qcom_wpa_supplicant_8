51e9d82bb686ef755abaae8420cf4640b0f1d9e4
Benjamin Berg <benjamin.berg@intel.com>
2023-05-22 22:33:31 +0300

    AP: MLO: Retrieve EML and MLD capabilities from driver
    Add a new driver API get_mld_capab() and and use it to fetch MLD and EML
    capabilities.
    
    Signed-off-by: Benjamin Berg <benjamin.berg@intel.com>
    CRs-Fixed: 3550303
    Git-repo: git://w1.fi/hostap.git
    Git-commit: 8dffa0ccb1fd13260401c0cfc5d11b640c838f5b
    Change-Id: Ie1beb6734fe37a43a485cbda687e0a76835caebf

--- src/ap/ap_drv_ops.h
+++ src/ap/ap_drv_ops.h
@@ -155,6 +155,7 @@ int hostapd_drv_set_qos_map(struct hostapd_data *hapd, const u8 *qos_map_set,
 			    u8 qos_map_set_len);
 
 void hostapd_get_ext_capa(struct hostapd_iface *iface);
+void hostapd_get_mld_capa(struct hostapd_iface *iface);
 
 static inline int hostapd_drv_set_countermeasures(struct hostapd_data *hapd,
 						  int enabled)
