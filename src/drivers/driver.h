e52b4b01780e2fcf01de3b636cca311dc5d898f5
Aloka Dixit <quic_alokad@quicinc.com>
2023-05-31 01:02:31 -0700

    EHT: Configure puncturing bitmap during channel switch
    Parse, validate, and configure puncturing bitmap if provided in the
    channel switch command.
    
    Signed-off-by: Aloka Dixit <quic_alokad@quicinc.com>
    
    Git-commit: 29a882bed324190f78f8bad53903319fc052e13e
    Git-repo: git://w1.fi/srv/git/hostap.git
    CRs-Fixed: 3479311
    Change-Id: I314f2035b2488e76e7265812f9583109f5020aec

--- src/drivers/driver.h
+++ src/drivers/driver.h
@@ -2570,6 +2570,7 @@ struct beacon_data {
  * @beacon_after: Next beacon/probe resp/asooc resp info
  * @counter_offset_beacon: Offset to the count field in beacon's tail
  * @counter_offset_presp: Offset to the count field in probe resp.
+ * @punct_bitmap - Preamble puncturing bitmap
  */
 struct csa_settings {
 	u8 cs_count;
@@ -2581,6 +2582,8 @@ struct csa_settings {
 
 	u16 counter_offset_beacon[2];
 	u16 counter_offset_presp[2];
+
+	u16 punct_bitmap;
 };
 
 /**
