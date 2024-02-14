686c2f4b8fc0a5e7c58b4e5251c6c5323cff4298
Ainy Kumari <quic_ainykuma@quicinc.com>
2024-02-14 13:05:40 +0530

    wpa_supplicant_8: Add support to update AP TID-to-link negotiation to Framework.
    Add support to update the Tid-to-Link mapping negotiation support
    by AP to the framework.
    
    Bug: 325048411
    Test: m
    Change-Id: I40c1f91b64c174d62e9b6c80c295d8a1015a521f
    CRs-Fixed: 3705720
    (Cherry-picked from 322d4fd240badfd9487375872ea5549079e57a84)

--- wpa_supplicant/wpa_supplicant_i.h
+++ wpa_supplicant/wpa_supplicant_i.h
@@ -999,6 +999,7 @@ struct wpa_supplicant {
 	unsigned int connection_channel_bandwidth:5;
 	unsigned int disable_mbo_oce:1;
 	unsigned int connection_11b_only:1;
+	unsigned int ap_t2lm_negotiation_support:1;
 
 	struct os_reltime last_mac_addr_change;
 	enum wpas_mac_addr_style last_mac_addr_style;
